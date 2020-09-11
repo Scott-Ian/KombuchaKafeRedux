50636d495de42f3416ad3ddc00b701d0b96c4bb1
Ian Scott <chesnekov@gmail.com>
2020-09-11 14:21:27 -0700

    add KombuchaDetail.js and content

---
+++ kombucha-kafe/src/components/KombuchaDetail.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KombuchaDetail() {
+  const {kombucha} = props
+
+  return(
+    <React.Fragment>
+      <h3>{kombucha.name} Details</h3>
+      <hr />
+      <p>{kombucha.description}</p>
+      <p>{kombucha.brewery}</p>
+      <p>${kombucha.price}</p>
+      <p>Remaining: {kombucha.quantity}</p>
+
+      <button className="btn btn-info" onClick={props.onClickingEdit}>Update Entry</button>
+      <button className="btn btn-alert" onClick={() => props.onClickingDelete(kombucha.id)}>Delete</button>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+KombuchaDetail.propTypes ={
+  kombucha: PropTypes.object,
+  onClickingEdit: PropTypes.func,
+  onClickingDelete: PropTypes.func
+}
+
+export default KombuchaDetail;
