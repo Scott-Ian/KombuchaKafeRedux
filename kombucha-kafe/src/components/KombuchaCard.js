72226f473e4a38420e11efd70960373008522002
Ian Scott <chesnekov@gmail.com>
2020-09-11 13:24:27 -0700

    add basic layout of a kombucha card component. Should have flip card capabilities

--- kombucha-kafe/src/components/KombuchaCard.js
+++ kombucha-kafe/src/components/KombuchaCard.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function KombuchaCard () {
+function KombuchaCard (props) {
   return(
     <React.Fragment>
-
+      <div className="flipCard" onClick = {props.whenKombuchaClicked(props.id)}>
+        <div className="flipCardFront">
+          <h3>{props.name}</h3>
+          <img src={props.imgURL} alt="Kombucha Picture" style="width:300px;height:300px;"></img>
+        </div>
+        <div className="flipCardBack">
+          <h3>{props.name}</h3>
+          <p>{props.brewery}</p>
+          <p>${props.price}</p>
+          <p>Remaining: {props.quantity}</p>
+          <button onClick={() => props.order(props.id)}>Order</button>
+        </div>
+      </div>
     </React.Fragment>
   );
+}
+
+KombuchaCard.propTypes = {
+  id: PropTypes.string, 
+  name: PropTypes.string,
+  brewery: PropTypes.string,
+  price: PropTypes.number,
+  description: PropTypes.string,
+  imgURL: PropTypes.string,
+  quantity: PropTypes.number,
+  order: PropTypes.func,
+  whenKombuchaClicked: PropTypes.func
 }
 
 export default KombuchaCard;
