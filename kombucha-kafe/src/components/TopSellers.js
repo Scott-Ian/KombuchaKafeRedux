e0e41f3bd1b100729dbeb7a4012569ac60033f67
Ian Scott <chesnekov@gmail.com>
2020-09-11 14:28:59 -0700

    update topSellers.js to display an orderd list of every item in the topSellers list

--- kombucha-kafe/src/components/TopSellers.js
+++ kombucha-kafe/src/components/TopSellers.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function TopSellers() {
+function TopSellers(props) {
   return(
     <React.Fragment>
-
+      <ol>
+        {props.topSellers.forEach((kombucha) =>
+          <li>{kombucha.name}</li>
+        )};
+      </ol>
     </React.Fragment>
   );
+}
+
+TopSellers.propTypes ={
+  topSellers: PropTypes.array,
 }
 
 export default TopSellers;
