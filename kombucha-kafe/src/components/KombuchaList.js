8fff3ea5745d12841c07153e39e5c52a74190bdf
Ian Scott <chesnekov@gmail.com>
2020-09-11 13:37:43 -0700

    add content for kombuchaList.js file to pass all necessary content down into a kombucha card

--- kombucha-kafe/src/components/KombuchaList.js
+++ kombucha-kafe/src/components/KombuchaList.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import KombuchaCard from '';
+import KombuchaCard from './KombuchaCard';
 
-function KombuchaList() {
+function KombuchaList(props) {
   return(
     <React.Fragment>
-
+      <hr/>
+      {props.kombuchaList.map((kombucha) =>
+        <KombuchaCard
+          whenKombuchaClicked = {props.onKombuchaSelection}
+          order={props.placeOrder}
+          name={kombucha.name}
+          brewery={kombucha.brewery}
+          price={kombucha.price}
+          description={kombucha.description}
+          imgURL={kombucha.imgURL}
+          quantity={kombucha.quantity}
+          id={kombucha.id}
+          key={kombucha.id} />
+      )}
     </React.Fragment>
   );
 }
