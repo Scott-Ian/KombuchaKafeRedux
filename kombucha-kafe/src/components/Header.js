2b73a1b238c13459eb1db884120b09ddf21749ca
Ian Scott <chesnekov@gmail.com>
2020-09-12 16:13:33 -0700

    fix error with TopSellers content not displaying

--- kombucha-kafe/src/components/Header.js
+++ kombucha-kafe/src/components/Header.js
@@ -1,21 +1,10 @@
 import React from 'react';
 
 function Header() {
-  const headerStyle = {
-    gridArea: "header"
-  };
-
-  const gridContainerStyle = {
-    display: "grid"
-  };
 
   return(
     <React.Fragment>
-      <div className="grid-container" style={gridContainerStyle}>
-        <div className= "header-label" style={headerStyle}>
           <h2>Kombucha Cafe</h2>
-        </div>
-      </div>
     </React.Fragment>
   );
 }
--- kombucha-kafe/src/components/TopSellers.js
+++ kombucha-kafe/src/components/TopSellers.js
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function TopSellers(props) {
-  const tempTopSellers = ["Honey Hibiscus", "Cheesy Chartruse", "Lavender Lemon"];
 
   return(
     <React.Fragment>
       <h2>Top Sellers:</h2>
       <ol>
-        {tempTopSellers.forEach((entry, index) =>
-          <li key={index}>{entry}</li>
+        {props.topSellers.map((entry) => 
+          <li key={entry.id}>{entry.name}</li>
         )}
       </ol>
     </React.Fragment>
