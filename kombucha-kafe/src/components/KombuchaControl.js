e2c937e3362362f6a6296a5c850cca6c7ed662fe
Ian Scott <chesnekov@gmail.com>
2020-09-12 16:30:07 -0700

    add additional kombucha entries

--- kombucha-kafe/src/components/KombuchaControl.js
+++ kombucha-kafe/src/components/KombuchaControl.js
@@ -30,6 +30,26 @@ class KombuchaControl extends React.Component {
         numberOrdered: 82,
         imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvTxYuLYKhs9h-yQW0MFTcS1hIIVFYsE0o_w&usqp=CAU",
         id: "2"
+      },
+      {
+        name: "Lu-Lu-Lucy",
+        brewery: "Lucy's Liquids",
+        price: 12,
+        quantity: 27,
+        description: "I love Lucy in a drink!",
+        numberOrdered: 12,
+        imgURL: "https://www.straight.com/files/v3/styles/gs_feature/public/images/16/08/o5_tea.jpg?itok=RvJ8b5Jo",
+        id: "3"
+      },
+      {
+        name: "Wolfe Cola",
+        brewery: "Franks Fluids",
+        price: 12,
+        quantity: 27,
+        description: "I love Lucy in a drink!",
+        numberOrdered: 12,
+        imgURL: "https://res.cloudinary.com/teepublic/image/private/s--Mdsbqva8--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_36538b,e_outline:48/co_36538b,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1512069827/production/designs/2123423_1.jpg",
+        id: "4"
       }],
       topSellers: [],
       selectedKombucha: null,
