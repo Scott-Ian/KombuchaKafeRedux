c442c23bd5251b2700f025444b6224561997fdda
Ian Scott <chesnekov@gmail.com>
2020-09-18 15:36:43 -0700

    reinstates create kombucha functionality, and returns to kombucha list component upon submission

--- src/components/KombuchaControl.js
+++ src/components/KombuchaControl.js
@@ -35,6 +35,14 @@ class KombuchaControl extends React.Component {
     dispatch(action);
     };
 
+    handleAddingNewKombuchaToList = (newKombucha) => {
+      const {dispatch } = this.props;
+      const action = a.addKombucha(newKombucha);
+      dispatch(action);
+      const action2 = a.listDisplay();
+      dispatch(action2);
+    }
+
 
   render() {
     let buttonText = null;
@@ -48,7 +56,7 @@ class KombuchaControl extends React.Component {
       pageToDisplay = <KombuchaDetail kombucha = {this.props.masterKombuchaList[selectedKombuchaId]}/>
       buttonText="Return to Kombucha List";
     } else if (this.props.displayState.display === 'CreateKombuchaForm') {
-      pageToDisplay = <CreateKombuchaForm />
+      pageToDisplay = <CreateKombuchaForm onNewKombuchaCreation={this.handleAddingNewKombuchaToList} />
       buttonText = "Return to Kombucha List";
     } else {
       pageToDisplay = <KombuchaList kombuchaList={this.props.masterKombuchaList} onKombuchaSelection = {this.handleChangingSelectedKombucha}/>
@@ -83,58 +91,6 @@ KombuchaControl = connect(mapStateToProps)(KombuchaControl);
 export default KombuchaControl;
 
 /*
-class KombuchaControl extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state={
-      formVisibleOnPage: false,
-      masterKombuchaList: [{
-        name: "Ginger Peach",
-        brewery: "Dave's Killer Kool-Aide",
-        price: 8,
-        quantity: 120,
-        description: "A tantalizing tase of ginger and peach awash the tongue in this beautiful concotion",
-        imgURL: "https://post.greatist.com/wp-content/uploads/2020/08/kombucha-alcohol-content-732x549-thumbnail.jpg",
-        numberOrdered: 31,
-        id: "1"
-      },
-      {
-        name: "Honey Hibiscus",
-        brewery: "Margaret's Manse",
-        price: 10,
-        quantity: 92,
-        description: "Your tastebuds will be blown away by this sweet drink with a floral aroma",
-        numberOrdered: 82,
-        imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvTxYuLYKhs9h-yQW0MFTcS1hIIVFYsE0o_w&usqp=CAU",
-        id: "2"
-      },
-      {
-        name: "Lu-Lu-Lucy",
-        brewery: "Lucy's Liquids",
-        price: 12,
-        quantity: 27,
-        description: "I love Lucy in a drink!",
-        numberOrdered: 12,
-        imgURL: "https://www.straight.com/files/v3/styles/gs_feature/public/images/16/08/o5_tea.jpg?itok=RvJ8b5Jo",
-        id: "3"
-      },
-      {
-        name: "Wolf Cola",
-        brewery: "Franks Fluids",
-        price: 12,
-        quantity: 27,
-        description: "I love Lucy in a drink!",
-        numberOrdered: 12,
-        imgURL: "https://res.cloudinary.com/teepublic/image/private/s--Mdsbqva8--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_36538b,e_outline:48/co_36538b,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1512069827/production/designs/2123423_1.jpg",
-        id: "4"
-      }],
-      topSellers: [],
-      selectedKombucha: null,
-      editing: false
-    }
-  }
-
   // switches formVisibleOnPage state
   handleClick = () => {
     if(this.state.selectedKombucha != null) {
