d4d7a9b2d3ff43ae95d18be795f200c3ca0b9f54
Ian Scott <chesnekov@gmail.com>
2020-09-18 10:22:00 -0700

    creates an addKombucha action constructor and an actionType of ADD_KOMBUCHA

--- src/_tests_/reducers/kombucha-list-reducer.test.js
+++ src/_tests_/reducers/kombucha-list-reducer.test.js
@@ -4,9 +4,24 @@ import * as c from './../../actions/ActionTypes';
 
 describe ('KombuchaListReducer', () => {
 
+  const kombuchaData = {
+    name: "Cherry Garcia",
+    brewery: "Greatful 'Bucha",
+    price: 9,
+    description: 'A cherry brew for the ages',
+    imgURL: '#',
+    quantity: 39,
+    numberOrdered: 18,
+    id: 66
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kombuchaListReducer({}, {type: null})).toEqual({});
   });
 
+  test('Should add a new Kombucha to the kombucha list', () => {
+    const action = addTicket(kombuchaData);
+    
+  });
 
 })
--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,18 @@ import * as c from './ActionTypes';
 export const toggleForm = () => ({
   type: c.TOGGLE_FORM
 });
+
+export const addKombucha = (kombucha) => {
+  const {name, brewery, price, description, imgURL, quantity, numberOrdered, id} = kombucha;
+  return {
+    type: c.ADD_KOMBUCHA,
+    name: name,
+    brewery: brewery,
+    price: price,
+    description: description,
+    imgURL: imgURL,
+    quantity: quantity,
+    numberOrdered,
+    id: id
+  }
+}
