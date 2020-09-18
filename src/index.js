c7039625eeae86dbb7516658459d60f5b23ebbd6
Ian Scott <chesnekov@gmail.com>
2020-09-18 08:26:55 -0700

    install redux and react-redux into project, updated index.js to have a store, and encapsulates app within Provider component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './components/serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
+
+store.subscribe( () =>
+  console.log(store.getState())
+  );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
