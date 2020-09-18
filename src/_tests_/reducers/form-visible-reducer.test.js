14f4c2018e333318582a708cd10c6b6f6b11c75d
Ian Scott <chesnekov@gmail.com>
2020-09-18 08:41:57 -0700

    add actionTYpe const for Toggle Form, add first test for formVisibleReducer which fails successfully'

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -1,1 +1,10 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
+import * as c from './../../actions/ActionTypes';
+
+describe ('formVisibleReducer', () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(formVisibleReducer(false, {type: null})).toEqual(false);
+  });
+
+})
